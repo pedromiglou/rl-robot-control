b3d2ee551aab3702271acd5c954613a1e8d0ef70
Pedro Amaral <pedromiglou@hotmail.com>
2024-04-23 19:11:42 +0100

    fetch_reach_joints_control

---
+++ fetch_reach_joints_control/env.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import gymnasium as gym
+import numpy as np
+
+from gymnasium_robotics.utils import mujoco_utils
+from gymnasium.spaces.discrete import Discrete
+from gymnasium.wrappers.monitoring.video_recorder import VideoRecorder
+
+
+class FetchReachJointsControl(gym.Env):
+    def __init__(self, record=False, **kwargs):
+        self.env = gym.make('FetchReachDense-v2', **kwargs)
+
+        self.joint_names = [
+            "robot0:shoulder_pan_joint",
+            "robot0:shoulder_lift_joint",
+            "robot0:upperarm_roll_joint",
+            "robot0:elbow_flex_joint",
+            "robot0:forearm_roll_link",
+            "robot0:wrist_flex_link",
+            "robot0:wrist_roll_joint"
+        ]
+        
+        self.record = record
+        if self.record:
+            self.video_recorder = VideoRecorder(
+                env=self.env,
+                base_path="./videos/discrete_fetch_reach"
+            )
+
+        # set discrete actions
+        self.discrete_actions = [[0,0,0,0,0,0,0]]
+
+        for i in range(7):
+            a = [0,0,0,0,0,0,0]
+            a[i] = 1
+            self.discrete_actions.append(a)
+
+            a = [0,0,0,0,0,0,0]
+            a[i] = -1
+            self.discrete_actions.append(a)
+
+        for i in range(len(self.discrete_actions)):
+            self.discrete_actions[i] = np.array(self.discrete_actions[i], dtype=np.float32)
+            if np.linalg.norm(self.discrete_actions[i]) != 0:
+                self.discrete_actions[i] *= 0.1 / np.linalg.norm(self.discrete_actions[i])
+            
+        self.action_space = Discrete(len(self.discrete_actions))
+        self.observation_space = self.env.observation_space
+
+    def step(self, action):
+        if self.record:
+            self.video_recorder.capture_frame()
+
+        x = mujoco_utils.robot_get_obs(self.env.model, self.env.data, self.joint_names)[0]
+        action = np.array(self.discrete_actions[action])
+
+        action += x
+
+        for i in range(7):
+            mujoco_utils.set_joint_qpos(self.env.model, self.env.data, self.joint_names[i], action[i])
+
+        action = np.array([0, 0, 0, 0])
+
+        return self.env.step(action)
+    
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+    
+    def render(self):
+        return self.env.render()
+    
+    def close(self):
+        if self.record:
+            self.video_recorder.capture_frame()
+            self.video_recorder.close()
+
+        self.env.close()
---
+++ fetch_reach_joints_control/train_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from stable_baselines3 import DQN
+from stable_baselines3.common.logger import configure
+
+from env import FetchReachJointsControl
+
+
+# load env
+env = FetchReachJointsControl(max_episode_steps=50)
+
+observation, info = env.reset(seed=42)
+
+new_logger = configure("./logs/", ["stdout", "csv"])
+
+# train model
+model = DQN("MultiInputPolicy", env, verbose=1)
+#model = DQN.load("dqn_fetch_reach")
+#model.set_env(env)
+model.set_logger(new_logger)
+model.learn(total_timesteps=1e6, log_interval=20)
+model.save("models/dqn_fetch_reach")
+
+print("training finished")
+
+env.close()
