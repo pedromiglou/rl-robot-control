f913a116265ee69f3e808f1c85b6cfa07dea091f
Pedro Amaral <pedromiglou@hotmail.com>
2024-04-24 17:45:40 +0100

    completely replace _set_action method to allow more end-effector orientations

---
+++ fetch_reach_joints_control/charts.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from scipy.interpolate import interp1d
+
+
+plt.rcParams['svg.fonttype'] = 'none'
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.size'] = 15
+
+
+def create_line_chart(x, y, xlabel, ylabel, avg_line = False, save_path=None):
+    # Create a figure and axis
+    plt.figure(figsize = (10,5))
+    
+    # Plot the line chart
+    plt.plot(x, y)
+
+    if avg_line:
+        x_new = []
+        y_new = []
+        
+        for i in range(10, len(y), 20):
+            x_new.append(x[i])
+            y_new.append(np.average(y[i-10:i+10]))
+        
+        cubic_interpolation_model = interp1d(x_new, y_new, kind = "cubic")
+        y_new=cubic_interpolation_model(x_new)
+
+        plt.plot(x_new, y_new, color='red')
+
+    
+    # Set the x-axis label
+    plt.xlabel(xlabel)
+    
+    # Set the y-axis label
+    plt.ylabel(ylabel)
+    
+    # Show or save the chart
+    if save_path:
+        plt.tight_layout()
+        plt.savefig(save_path)
+    
+    else:
+        plt.show()
+
+
+# Specify the file path
+file_path = './logs/progress.csv'
+
+# Read the CSV file
+df = pd.read_csv(file_path)
+
+# Call the function with the DataFrame
+create_line_chart(df["time/episodes"], df["train/loss"], "Episodes", "Loss", avg_line = True, save_path="./logs/loss.svg")
+
+create_line_chart(df["time/episodes"], df["rollout/ep_rew_mean"], "Episodes", "Episode Reward Mean", avg_line = True, save_path="./logs/reward.svg")
+
+create_line_chart(df["time/episodes"], df["rollout/exploration_rate"], "Episodes", "Exploration Rate", save_path="./logs/exploration_rate.svg")
+
+create_line_chart(df["time/episodes"], df["rollout/success_rate"], "Episodes", "Success Rate", avg_line = True, save_path="./logs/success_rate.svg")
--- fetch_reach_joints_control/env.py
+++ fetch_reach_joints_control/env.py
@@ -4,13 +4,34 @@ import gymnasium as gym
 import numpy as np
 
 from gymnasium_robotics.utils import mujoco_utils
+from gymnasium_robotics.envs.fetch.reach import MujocoFetchReachEnv
+from gymnasium.envs.registration import register
 from gymnasium.spaces.discrete import Discrete
 from gymnasium.wrappers.monitoring.video_recorder import VideoRecorder
 
 
+class CustomMujocoFetchReachEnv(MujocoFetchReachEnv):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def _set_action(self, action):
+        pass
+        #print("gg")
+
+register(
+    # unique identifier for the env `name-version`
+    id='FetchReachDense-custom',
+    # path to the class for creating the env
+    # Note: entry_point also accept a class as input (and not only a string)
+    entry_point="env:CustomMujocoFetchReachEnv",
+    # Max number of steps per episode, using a `TimeLimitWrapper`
+    max_episode_steps=50,
+)
+
+
 class FetchReachJointsControl(gym.Env):
     def __init__(self, record=False, **kwargs):
-        self.env = gym.make('FetchReachDense-v2', **kwargs)
+        self.env = gym.make('FetchReachDense-custom', **kwargs)
 
         self.joint_names = [
             "robot0:shoulder_pan_joint",
@@ -56,10 +77,13 @@ class FetchReachJointsControl(gym.Env):
         x = mujoco_utils.robot_get_obs(self.env.model, self.env.data, self.joint_names)[0]
         action = np.array(self.discrete_actions[action])
 
+        #print(x)
         action += x
+        #print(action)
 
-        for i in range(7):
+        for i in range(7):            
             mujoco_utils.set_joint_qpos(self.env.model, self.env.data, self.joint_names[i], action[i])
+            mujoco_utils.reset_mocap2body_xpos(self.env.model, self.env.data)
 
         action = np.array([0, 0, 0, 0])
 
