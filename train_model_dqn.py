d4c0919789016ba5ab7f8afd19a2871714afdefd
Pedro Amaral <pedromiglou@hotmail.com>
2024-05-13 17:35:46 +0100

    make eval envs deterministic

--- train_model_dqn.py
+++ train_model_dqn.py
@@ -20,12 +20,12 @@ env_logger = configure(RESULTS_FOLDER, ["stdout", "csv"])
 # create env
 logger.info("Creating environment...")
 env = FetchReachCartesianDiscrete(max_episode_steps=50, render_mode=None)
-eval_env = FetchReachCartesianDiscrete(max_episode_steps=50, render_mode=None, deterministic=True)
+eval_env = FetchReachCartesianDiscrete(max_episode_steps=50, render_mode=None)
 
 # Stop training if there is no improvement after more than 10 evaluations
 logger.info("Setting up callbacks...")
 stop_train_callback = StopTrainingOnNoModelImprovement(max_no_improvement_evals=10, min_evals=5, verbose=1)
-eval_callback = EvalCallback(eval_env, eval_freq=500*50, n_eval_episodes=50, callback_after_eval=stop_train_callback, verbose=1, best_model_save_path=RESULTS_FOLDER)
+eval_callback = EvalCallback(eval_env, eval_freq=500*50, n_eval_episodes=50, callback_after_eval=stop_train_callback, verbose=1, best_model_save_path=RESULTS_FOLDER, deterministic=True)
 
 # train model
 logger.info("Starting model training...")
